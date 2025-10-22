f7629c7a2d800b08b574990103572499efd8e861
jaeyoungchoi1 <choijy5847@naver.com>
2025-10-22 11:35:34 +0900

    feat: autodate

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  define: {
+    __BUILD_DATE__: JSON.stringify(new Date().toLocaleDateString('en-US', { 
+      month: 'short', 
+      year: 'numeric' 
+    }))
+  }
 })
